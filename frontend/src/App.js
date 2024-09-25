99224acb798a05fe83afb92e37c9c15e5d983fb6
Prabir <prabirsacademy@gmail.com>
2024-09-25 17:00:11 +0530

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,18 @@
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom"
 const App = () => {
-  return <h1>Welcome to ProShop</h1>
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Navbar, Nav, Container, NavbarToggle } from "react-bootstrap"
+import { FaShoppingCart, FaUser } from "react-icons/fa"
+import logo from "../assets/logo.png"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="proshop" />
+            ProShop
+          </Navbar.Brand>
+          <NavbarToggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+export default Header
